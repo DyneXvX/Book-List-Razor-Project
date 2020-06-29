647dc4a94d53374ba105f349b9159f811465d8e2
Justin Thoms <DyneXvX@outlook.com>
2020-06-29 16:14:13 -0400

    Setup Database Connection String, built SQL database, starting building Book List Index

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }//DataBase Connection
+
+        public IEnumerable<Book> Books { get; set; }
+        //Build the IEnumerable of Books
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }//get the list of books.
+        //Async let you run multiple tasks at a time till it is awaiting. 
+    }
+}
